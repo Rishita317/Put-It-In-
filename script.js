ba26bb73c16af4a44c3795d02a88c18b76917a1e
Rishita Meharishi <124011620+Rishita317@users.noreply.github.com>
2024-04-07 10:46:56 -0700

    Hackathon
    Put it in

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+        mapId: '177d9d81cfb4d163'
+    });
+}
